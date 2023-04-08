e9cef95b13125554287fc13d9b6e0fd9f48e0f2a
Vikesh Varsani <vikon1998@gmail.com>
2023-04-08 20:35:03 +0800

    Added tests for initialization of Accounts

--- tests/unit/test_accounts.py
+++ tests/unit/test_accounts.py
@@ -9,10 +9,10 @@ from src.accounts import Account
         ("John", "Doe", 1, 10.0, {"id": 1, "first_name": "John", "last_name": "Doe", "rate": 10.0}),
         (
             "X",
-            "Y_@()!1",
+            "Y",
             165468787,
             68.56,
-            {"id": 165468787, "first_name": "X", "last_name": "Y_@()!1", "rate": 68.56},
+            {"id": 165468787, "first_name": "X", "last_name": "Y", "rate": 68.56},
         ),
     ],
 )
