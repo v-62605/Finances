23fc0bb0dd44c456994a55c41787837cd9de1a8f
Vikesh Varsani <vikon1998@gmail.com>
2023-04-08 21:52:41 +0800

    Added test for daily accounting sequences

--- tests/unit/test_accounts.py
+++ tests/unit/test_accounts.py
@@ -21,3 +21,57 @@ def test_account_init(
 ) -> None:
     account = Account(id, first_name, last_name, rate)
     assert account.get_info() == expected
+
+
+@pytest.mark.parametrize(
+    "first_name, last_name, rate, expected",
+    [
+        (
+            "John",
+            "Doe",
+            10.0,
+            {
+                "first": {
+                    "unpaid_hours": 20,
+                    "paid_hours": 20,
+                    "unpaid_income": 200,
+                    "paid_income": 200,
+                },
+                "second": {
+                    "unpaid_hours": 0,
+                    "paid_hours": 50,
+                    "unpaid_income": 0,
+                    "paid_income": 800,
+                },
+            },
+        ),
+        (
+            "X",
+            "Y",
+            60,
+            {
+                "first": {
+                    "unpaid_hours": 20,
+                    "paid_hours": 20,
+                    "unpaid_income": 1200,
+                    "paid_income": 1200,
+                },
+                "second": {
+                    "unpaid_hours": 0,
+                    "paid_hours": 50,
+                    "unpaid_income": 0,
+                    "paid_income": 3300,
+                },
+            },
+        ),
+    ],
+)
+def test_sequence_of_daily_accounting_events(
+    first_name: str, last_name: str, rate: float, expected: dict
+) -> None:
+    account = Account(0, first_name, last_name, rate)
+    account.add_hours(40)
+    assert account.pay(20) == expected["first"]
+    account.update_rate(rate + 10)
+    account.add_hours(10)
+    assert account.pay() == expected["second"]
