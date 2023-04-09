9e5c65af2aea064550c00cf02ae6e01a4447c48f
Vikesh Varsani <vikon1998@gmail.com>
2023-04-09 14:38:18 +0800

    Added generating of random accounts

--- src/accounts.py
+++ src/accounts.py
@@ -1,23 +1,25 @@
 import random
 import time
-from typing import Union
+
+import names
 
 
 class Account:
-    def __init__(self, id: int, first_name: str, last_name: str, rate: Union[float, None] = None):
+    def __init__(self, id: int, first_name: str, last_name: str, rate: float):
         random.seed(time.time())
-        self.id = random.randrange(1000000000, 99999999999999999) if id <= 0 else id
+        self.id = random.randrange(1_000_000_000, 99999999999999999) if id <= 1_000_000_000 else id
         self.first_name = first_name
         self.last_name = last_name
-        self.rate = rate
+        self.rate = rate if rate is not None else None
         self.unpaid_hours: float = 0
         self.unpaid_income: float = 0
         self.paid_hours: float = 0
         self.paid_income: float = 0
         self.average_pay_rate: float = 0
+        self.pay_raise: list = []
 
     def __repr__(self) -> str:
-        return f"Account({self.id}, {self.first_name}, {self.last_name})"
+        return f"Account({self.id}, {self.first_name}, {self.last_name}, {self.rate})"
 
     def __str__(self) -> str:
         return f"{self.first_name} {self.last_name}"
@@ -34,6 +36,10 @@ class Account:
             "first_name": self.first_name,
             "last_name": self.last_name,
             "rate": self.rate,
+            "unpaid_hours": self.unpaid_hours,
+            "unpaid_income": self.unpaid_income,
+            "paid_hours": self.paid_hours,
+            "paid_income": self.paid_income,
         }
 
     def update_rate(self, rate: float) -> dict:
@@ -57,6 +63,7 @@ class Account:
             raise ValueError("Rate is not set")
 
         self.unpaid_hours += hours
+        self.unpaid_income = self.unpaid_hours * self.rate
         return {"unpaid_hours": self.unpaid_hours, "paid_hours": self.paid_hours}
 
     def get_income(self) -> dict:
@@ -83,3 +90,38 @@ class Account:
             "unpaid_income": self.unpaid_income,
             "paid_income": self.paid_income,
         }
+
+    def save(self) -> None:
+        pass
+
+
+def generate_accounts(n: int) -> list:
+    accounts = []
+    for i in range(n):
+        accounts.append(
+            Account(
+                i,
+                names.get_first_name(),
+                names.get_last_name(),
+                round(random.normalvariate(30, 5), 2),
+            )
+        )
+
+    return accounts
+
+
+def generate_daily_events_for_an_account(account: Account) -> Account:
+    """Generate daily events for an account"""
+
+    # Add hours for the day
+    account.add_hours(round(random.uniform(8, 10), 2))
+
+    # Get a raise with probability 0.05
+    if random.random() < 0.01:
+        account.update_rate(round(account.rate * (1 + random.uniform(0, 0.25)), 2))
+
+    # Pay when the account has more than 40 hours
+    if account.unpaid_hours >= 40:
+        account.pay()
+
+    return account
